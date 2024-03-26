e6455f45a7644c90db849ada9fe51c30dde48e3c
Johnesli <johnesli40@gmail.com>
2024-03-26 14:34:39 +0530

    FIX : Some changes

--- static/invclc/js/index.js
+++ static/invclc/js/index.js
@@ -82,7 +82,7 @@ function openpanel(evt, panels) {
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
-    tabcontent[i].style.backgroundColor="rgba(172, 172, 172,0.4)";
+    tabcontent[i].style.backgroundColor="rgb(83 78 78 / 60%)";
   }
   tablinks = document.getElementsByClassName("tablinks");
   for (i = 0; i < tablinks.length; i++) {
@@ -95,7 +95,7 @@ function openpanel(evt, panels) {
   }
   if (evt.currentTarget.className.indexOf("active") === -1) {
     evt.currentTarget.className += " active";
-    evt.currentTarget.style.backgroundColor = "rgba(172, 172, 172,0.4)";
+    evt.currentTarget.style.backgroundColor = "rgb(83 78 78 / 60%)";
   }
   else {
     evt.currentTarget.style.backgroundColor = "#fff"; 
@@ -320,6 +320,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
           // Reload the page after successful payment
           location.reload();
+          
         })
         .catch(error => {
           console.error('Error updating row:', error);
