eb2f2a8387501e58cd4ea7d36d3a303c161235d5
Johnesli <johnesli40@gmail.com>
2024-04-06 12:45:56 +0530

    final changes2

--- invclc/forms.py
+++ invclc/forms.py
@@ -19,6 +19,20 @@ class InvoiceForm(forms.ModelForm):
         widget=forms.DateInput(format='%d/%m/%Y', attrs={'placeholder': 'DD/MM/YYYY', 'title': 'Default today Date'})
     )
 
+    class Meta:
+        model = Invoice
+        fields = ['pharmacy_name', 'invoice_number', 'invoice_date', 'invoice_amount', 'balance_amount', 'payment_amount', 'today_date', 'current_time', 'updated_by']
+        widgets = {
+            'pharmacy_name': forms.TextInput(attrs={'placeholder': 'Pharmacy Name','class':'pharmacy_name'}),
+            'invoice_number': forms.TextInput(attrs={'placeholder': 'Invoice Number','value':'IV-','class':'invoice_number'}),
+            'invoice_amount': forms.NumberInput(attrs={'placeholder': 'Invoice Amount','class':'invoice_amount'}),
+            'balance_amount': forms.TextInput(attrs={'placeholder': 'Balance Amount', 'disabled': True,'class':'balance_amount'}),
+            'payment_amount': forms.NumberInput(attrs={'placeholder': '0','title':'Default 0 ','class':'payment_amount'}),
+            'today_date':forms.DateInput(attrs={'type':'hidden'}),
+            'current_time':forms.TimeInput(attrs={'type':'hidden'}),
+            'updated_by':forms.TextInput(attrs={'type':'hidden',}),
+        }
+
 class UploadFileForm(forms.Form):
     file = forms.FileField(label='Select a file')
 
