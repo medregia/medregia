d33e90d4b5d146f5b65175468fe8e34d13f259da
Murugan25mm <murugan25122000@gmail.com>
2024-04-12 13:46:24 +0530

    feat : add html file

--- static/invclc/js/import-export.js
+++ static/invclc/js/import-export.js
@@ -12,8 +12,8 @@ function opentab(evt, cityName) {
   tablinks = document.getElementsByClassName("tablinks");
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
+    
   }
-
   // Show the current tab, and add an "active" class to the button that opened the tab
   document.getElementById(cityName).style.display = "block";
   evt.currentTarget.className += " active";
