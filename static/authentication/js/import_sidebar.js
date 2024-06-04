e24074537f90528a11458f93832539b69a20f9d6
Sailappan <sailsiva13@gmail.com>
2024-06-04 13:27:28 +0530

    updatin1

--- static/authentication/js/import_sidebar.js
+++ static/authentication/js/import_sidebar.js
@@ -1,14 +1,11 @@
 function toggleSidebar() {
     var sidebar = document.getElementById('sidebar');
-    var medregiaHeader = document.getElementById('medregiaHeader');
+    // var medregiaHeader = document.getElementById('medregiaHeader');
     var imp_exp_content = document.getElementById('imp_exp_content');
     
     if (sidebar.classList.contains('sidebar-closed')) {
         sidebar.classList.remove('sidebar-closed');
         sidebar.classList.add('sidebar-open');
-        medregiaHeader.style.display = 'block';
-        medregiaHeader.style.opacity = '1'; // Show slowly
-        medregiaHeader.style.transition = 'opacity 1s';
         imp_exp_content.style.marginLeft = '150px'; // Adjust this value according to your sidebar width
         imp_exp_content.style.width = '89%';
         // mainContent.style.transition = '0.3s';
@@ -16,9 +13,6 @@ function toggleSidebar() {
     } else {
         sidebar.classList.remove('sidebar-open');
         sidebar.classList.add('sidebar-closed');
-        medregiaHeader.style.display = 'none';
-        medregiaHeader.style.opacity = '0'; // Hide slowly
-        medregiaHeader.style.transition = 'opacity 1s';
         imp_exp_content.style.marginLeft = '50px';
         imp_exp_content.style.width = '96%';
     }
