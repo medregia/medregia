454d3cf99c0f96fe1712a6f2572cc25959f9bb9d
Johnesli <johnesli40@gmail.com>
2024-06-16 21:37:59 +0530

    Notification Connection

--- authentication/context_processors.py
+++ authentication/context_processors.py
@@ -13,6 +13,7 @@ def nav_message(request):
     if request.user.is_authenticated:
         current_user = request.user
         notify_message = None
+        incomplete_message = None
         
         notifications = ConnectMedicals.objects.filter(request_receiver=current_user, is_read=False, accept_status=True)
         
@@ -21,8 +22,10 @@ def nav_message(request):
                 check_profile = Person.objects.get(user=current_user)
                 profile_uniqueId = check_profile.UniqueId
 
-                if not profile_uniqueId:
+                if not profile_uniqueId or '####' in profile_uniqueId:
                     notify_message = True
+                    if '####' in profile_uniqueId:
+                        incomplete_message = "Your profile is still incomplete. Register the address section on your profile to access the request."
                 else:
                     notify_message = False
                     
@@ -35,4 +38,4 @@ def nav_message(request):
         notifications = []
         notify_message = None
     
-    return {'notifications': notifications, 'notify_message': notify_message}
+    return {'notifications': notifications, 'notify_message': notify_message, 'incomplete_message': incomplete_message}
